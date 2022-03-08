2dc3a18cde10a767b8be8c1a6cd2fefc01d94479
Cameron Perera <cameronperera@cameronperera-c02zq2afmd6r.mediacom.info>
2022-03-08 12:00:45 -0600

    Initial commit

---
+++ app/routes/bands.js
@@ -0,0 +1,20 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import Band from 'rarwe/models/band';
+import fetch from 'fetch';
+
+export default class BandsRoute extends Route {
+  @service catalog;
+
+  async model() {
+    let response = await fetch('/bands');
+    let json = await response.json();
+    console.log({ json });
+    for (let item of json.data) {
+      let { id, attributes } = item;
+      let record = new Band({ id, ...attributes });
+      this.catalog.add('band', record);
+    }
+    return this.catalog.bands;
+  }
+}
